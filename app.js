4c9803a14c082dd0e8f8fdb4ceae3ae46c0f1c87
NaveedHussainCodes <141583319+NaveedHussainCodes@users.noreply.github.com>
2026-04-28 18:55:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+// for (let i = 0; i <= 100; i++) {
+//     if (i%2 !== 0) {
+//         console.log(i);
+        
+//     }
+    
+// }
+
+// let arr = ["Naveed","Ahmed","fareed","Shazil"]
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+    
+    
+// }
+
+let marks =  [85, 97, 44, 37, 76, 60];
+ 
+let sum = 0;
+// for (const val of marks) {
+//     sum += val;
+// }
+// console.log(sum);
+
+// for (let i = 0; i < marks.length; i++) {
+//     console.log(sum += ma);
+    
+    
+// }
+
+// function navi(b,c) {
+//     return b+c;
+    
+// }
+//  let s = navi(3,4);
+//  console.log(s);
+
+//  let arr = (a,b) => {
+//     return a+b;
+//  }
+
+let student = [
+    {name: "Naveed", age: 23, marks: 76},
+    {name: "Ahmed", age: 16, marks: 86},
+    {name: "Shazil", age: 20, marks: 98},
+];
+ 
+// console.log(student[0].name);
+// console.log(student[1].age);
+// console.log(student[2].marks);
+
+// for (let i = 0; i < student.length; i++) {
+//     console.log(student[i].name + "-"+ student[i].marks);
+    
+// }
+
+// student.forEach(function (data) {
+//     console.log(data.name+"-"+data.marks);
+    
+// })
+
+// const passing = student.filter(function (student) {
+//     return student.age >= 19;
+// })
+// console.log(passing);
+
+// let passing = student.filter(function(student) {
+//     return student.marks >= 80;
+// });
+
+// console.log(passing);
+ 
+let hi = document.querySelector("h2");
+hi.tagName("p")
